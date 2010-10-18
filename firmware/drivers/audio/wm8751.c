eisnerd/rockbox

commit 23f61b69e6979a841a11fd216d3d66e2c1912ff7

HD200 - GPIO57 is headphone output enable. Proper handling of this line fixes hissing noise after startup.
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@28270 a1c6a512-1295-4272-9138-f99709370657

firmware/drivers/audio/wm8751.c
@@ -190,6 +190,15 @@ void audiohw_preinit(void)
     GPIOL_OUTPUT_VAL |= 0x10; /* disable */
 #endif
 
+#ifdef MPIO_HD200
+    /* control headphone output
+     * disabled on startup
+     */
+    and_l((1<<25),&GPIO1_OUT);
+    or_l((1<<25), &GPIO1_ENABLE);
+    or_l((1<<25), &GPIO1_FUNCTION);
+#endif
+
     /*
      * 1. Switch on power supplies.
      *    By default the WM8751 is in Standby Mode, the DAC is
@@ -274,6 +283,11 @@ void audiohw_postinit(void)
     GPIOL_OUTPUT_VAL &= ~0x10;
     GPIOL_OUTPUT_EN  |=  0x10;
 #endif
+
+#ifdef MPIO_HD200
+   /* enable headphone output */
+   or_l((1<<25),&GPIO1_OUT);
+#endif
 }
 
 void audiohw_set_master_vol(int vol_l, int vol_r)
@@ -326,6 +340,11 @@ void audiohw_close(void)
 {
     /* 1. Set DACMU = 1 to soft-mute the audio DACs. */
     audiohw_mute(true);
+
+#ifdef MPIO_HD200
+    /* disable headphone out */
+    and_l(~(1<<25), &GPIO1_OUT);
+#endif
 
     /* 2. Disable all output buffers. */
     wmcodec_write(PWRMGMT2, 0x0);

firmware/target/coldfire/crt0.S
@@ -278,7 +278,7 @@ start:
 
 #ifdef MPIO_HD200
     /* Set KEEP_ACT */
-    move.l #0x02200000,%d0
+    move.l #0x00200000,%d0
     or.l %d0,(0xb4,%a1)
     or.l %d0,(0xb8,%a1)
     or.l %d0,(0xbc,%a1)

commit 2bc8fb3268ad57b91427c111f214e266e4094b2d

MPIO HD200 - fix stupid typo introduced in r28270 which forced device hard shutdown during boot.
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@28304 a1c6a512-1295-4272-9138-f99709370657

firmware/drivers/audio/wm8751.c
@@ -194,7 +194,7 @@ void audiohw_preinit(void)
     /* control headphone output
      * disabled on startup
      */
-    and_l((1<<25),&GPIO1_OUT);
+    and_l(~(1<<25),&GPIO1_OUT);
     or_l((1<<25), &GPIO1_ENABLE);
     or_l((1<<25), &GPIO1_FUNCTION);
 #endif